2924381157e46d2257c12b93832931d388cb38f1
notabhishek <notabhishektiwari@gmail.com>
2022-01-20 16:36:07 +0530

    code cleanup

--- springbootapp/src/main/java/com/flock/springbootbackend/SpringbootBackendApplication.java
+++ springbootapp/src/main/java/com/flock/springbootbackend/SpringbootBackendApplication.java
@@ -2,8 +2,6 @@ package com.flock.springbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
