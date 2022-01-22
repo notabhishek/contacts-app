c4d3976026597e01118da868eda924cc0d8e28f3
notabhishek <notabhishektiwari@gmail.com>
2022-01-22 14:47:20 +0530

    update imports

--- springbootapp/src/main/java/com/flock/springbootbackend/controller/AuthController.java
+++ springbootapp/src/main/java/com/flock/springbootbackend/controller/AuthController.java
@@ -5,16 +5,20 @@ import com.flock.springbootbackend.requestObjects.LoginCredentials;
 import com.flock.springbootbackend.model.User;
 import com.flock.springbootbackend.repository.UserRepo;
 import com.flock.springbootbackend.security.JWTUtil;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Collections;
 import java.util.Map;
 
+import static com.flock.springbootbackend.Utils.AuthContants.INVALID_LOGIN_CREDENTIALS;
 import static com.flock.springbootbackend.Utils.AuthContants.JWT_TOKEN;
 
 @RestController
@@ -29,16 +33,18 @@ public class AuthController {
     @Autowired private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public Map<String, Object> registerHandler(@RequestBody User user){
-        String encodedPass = passwordEncoder.encode(user.getPassword());
+    public Map<String, Object> registerHandler(@RequestBody User user) {
+        try {
 
-        user.setPassword(encodedPass);
+            String encodedPass = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPass);
+            user = userRepo.save(user);
+            String token = jwtUtil.generateToken(user.getEmail());
+            return Collections.singletonMap(JWT_TOKEN, token);
 
-        user = userRepo.save(user);
-
-        String token = jwtUtil.generateToken(user.getEmail());
-
-        return Collections.singletonMap(JWT_TOKEN, token);
+        } catch (DataIntegrityViolationException e) {
+            return Collections.singletonMap("Error", "User with this email already exists");
+        }
     }
 
     @PostMapping("/login")
@@ -53,7 +59,8 @@ public class AuthController {
 
             return Collections.singletonMap(JWT_TOKEN, token);
         }catch (AuthenticationException authExc){
-            throw new RuntimeException(Utils.AuthContants.INVALID_LOGIN_CREDENTIALS);
+            return Collections.singletonMap("Error", INVALID_LOGIN_CREDENTIALS);
+//            throw new RuntimeException(Utils.AuthContants.INVALID_LOGIN_CREDENTIALS);
         }
     }
 }
